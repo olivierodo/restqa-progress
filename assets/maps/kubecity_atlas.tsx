8f27005e4d1f6dc60d828ea69508610e0b8d2279
Olivier <hi@olivierodo.me>
2022-08-14 23:18:16 +0700

    update

---
+++ assets/maps/kubecity_atlas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="kubecity_atlas" tilewidth="32" tileheight="32" tilecount="144" columns="16">
+ <image source="kubecity_atlas.png" width="512" height="288"/>
+</tileset>
